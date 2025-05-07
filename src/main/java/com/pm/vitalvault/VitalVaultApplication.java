ce813e16ab3d33b68fe199fb868b3abb32a2381a
Lauren Bennett <labenne3@ncsu.edu>
2025-05-07 17:33:28 -0400

    barebones

---
+++ src/main/java/com/pm/vitalvault/VitalVaultApplication.java
@@ -0,0 +1,13 @@
+package com.pm.vitalvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitalVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pm/vitalvault/VitalVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.vitalvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
